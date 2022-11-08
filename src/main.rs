fe7faefc4b64c460a4e0d63893d33dcb71684227
Felix Weilbach <felix.weilbach@t-online.de>
2022-11-08 22:22:39 +0100

    Draw lines

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use winit::event_loop::EventLoop;
 use winit::window::WindowBuilder;
 use winit_input_helper::WinitInputHelper;
 
+type Color = (u8, u8, u8);
+type PixelPosition = (u16, u16);
+
 fn main() {
     let mut input = WinitInputHelper::new();
     let event_loop = EventLoop::new();
@@ -23,18 +26,35 @@ fn main() {
                 (size.width, size.height)
             };
             draw_state.resize(width as u16, height as u16);
+            draw_state.clear();
 
-            for index in 0..((width * height) as usize) {
-                let y = index / (width as usize);
-                let x = index % (width as usize);
-                let red = x % 255;
-                let green = y % 255;
-                let blue = (x * y) % 255;
-                draw_state.set_pixel_rgb(
-                    (x as u16, y as u16),
-                    (red as u8, green as u8, blue as u8),
-                );
-            }
+            draw_line(
+                &mut draw_state,
+                (0, 0),
+                ((width - 1) as u16, (height - 1) as u16),
+                (255, 255, 255),
+            );
+
+            draw_line(
+                &mut draw_state,
+                (0, (height - 1) as u16),
+                ((width - 1) as u16, 0),
+                (255, 255, 255),
+            );
+
+            draw_line(
+                &mut draw_state,
+                (0, (height - 1) as u16 / 2),
+                ((width - 1) as u16, (height - 1) as u16 / 2),
+                (255, 255, 255),
+            );
+
+            draw_line(
+                &mut draw_state,
+                ((width - 1) as u16 / 2, 0),
+                ((width - 1) as u16 / 2, (height - 1) as u16),
+                (255, 255, 255),
+            );
 
             graphics_context.set_buffer(
                 draw_state.buffer(),
@@ -45,11 +65,58 @@ fn main() {
     });
 }
 
+fn draw_line(
+    draw_state: &mut DrawState,
+    start: PixelPosition,
+    end: PixelPosition,
+    color: Color,
+) {
+    use std::mem::swap;
+
+    let mut x0 = start.0 as i32;
+    let mut y0 = start.1 as i32;
+
+    let mut x1 = end.0 as i32;
+    let mut y1 = end.1 as i32;
+
+    let steep = if (x0 - x1).abs() < (y0 - y1).abs() {
+        swap(&mut x0, &mut y0);
+        swap(&mut x1, &mut y1);
+        true
+    } else {
+        false
+    };
+
+    if x0 > x1 {
+        swap(&mut x0, &mut x1);
+        swap(&mut y0, &mut y1);
+    }
+
+    let dx = x1 - x0;
+    let dy = y1 - y0;
+    let derror2 = dy.abs() * 2;
+    let mut error2 = 0;
+    let mut y = y0;
+    for x in x0..x1 {
+        if steep {
+            draw_state.set_pixel_rgb((y as u16, x as u16), color);
+        } else {
+            draw_state.set_pixel_rgb((x as u16, y as u16), color);
+        }
+        error2 += derror2;
+        if error2 > dx {
+            y += if y1 > y0 { 1 } else { -1 };
+            error2 -= dx * 2;
+        }
+    }
+}
+
 /// Holds a framebuffer for drawing
 struct DrawState {
     framebuffer: Vec<u32>,
-    width: u16,
-    height: u16,
+    width: usize,
+    height: usize,
+    clear_color: Color,
 }
 
 impl DrawState {
@@ -59,22 +126,37 @@ impl DrawState {
             framebuffer: Vec::new(),
             width: 0,
             height: 0,
+            clear_color: (0, 0, 0),
         }
     }
 
     /// Resizes the framebuffer if the width and height does not match
     pub fn resize(&mut self, width: u16, height: u16) {
+        let width = width as usize;
+        let height = height as usize;
+
         if self.width != width || self.height != height {
             self.width = width;
             self.height = height;
 
-            self.framebuffer
-                .resize(self.width as usize * self.height as usize, 0);
+            self.framebuffer.resize(self.width * self.height, 0);
+        }
+    }
+
+    /// Clears the background to the clear color
+    pub fn clear(&mut self) {
+        let red = self.clear_color.0 as u32;
+        let green = self.clear_color.1 as u32;
+        let blue = self.clear_color.2 as u32;
+        let color = blue | (green << 8) | (red << 16);
+
+        for i in 0..self.width * self.height {
+            self.framebuffer[i] = color;
         }
     }
 
     /// Sets the pixel at the given position to the specified RGB color
-    pub fn set_pixel_rgb(&mut self, pos: (u16, u16), rgb: (u8, u8, u8)) {
+    pub fn set_pixel_rgb(&mut self, pos: PixelPosition, rgb: Color) {
         let x = pos.0 as usize;
         let y = pos.1 as usize;
 
@@ -84,7 +166,7 @@ impl DrawState {
 
         let color = blue | (green << 8) | (red << 16);
 
-        self.framebuffer[(y * self.width as usize) + x] = color;
+        self.framebuffer[(y * self.width) + x] = color;
     }
 
     /// Returns a reference to the framebuffer
